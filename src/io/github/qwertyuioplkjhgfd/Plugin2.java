b4a0f35e8627adf4b58fdc8ccd8e5a05139b606c
qwertyuioplkjhgfd <32070@students.ausd.net>
2020-10-04 11:06:39 -0700

    ill just make a new repo lol

---
+++ src/io/github/qwertyuioplkjhgfd/Plugin2.java
@@ -0,0 +1,49 @@
+package io.github.qwertyuioplkjhgfd;
+
+import io.github.qwertyuioplkjhgfd.listeners.PlayerDeathListener;
+import org.bukkit.Server;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Plugin2 extends JavaPlugin {
+    Server server = getServer();
+    @Override
+    public void onEnable() {
+        System.out.println("lol idk");
+        new PlayerDeathListener(this);
+
+    }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if(label.equalsIgnoreCase("test")) {
+            if(sender instanceof Player) {
+                Player player = (Player)sender;
+                player.sendMessage("if this works i'm gonna go sleep");
+                player.chat("hi lol");
+                server.broadcastMessage("broadcast");
+                return true;
+            }
+        }
+        if(label.equalsIgnoreCase("heal")) {
+            if(sender instanceof Player) {
+                Player player = (Player)sender;
+                /*
+                player.setHealth(20);
+                player.setFoodLevel(20);
+                player.setSaturation(20);
+
+                 */
+            }
+        }
+        return false;
+    }
+
+
+
+    @Override
+    public void onDisable() {
+
+    }
+}
---
+++ src/io/github/qwertyuioplkjhgfd/listeners/PlayerDeathListener.java
@@ -0,0 +1,66 @@
+package io.github.qwertyuioplkjhgfd.listeners;
+
+import io.github.qwertyuioplkjhgfd.Plugin2;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Fireball;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class PlayerDeathListener implements Listener {
+    private static Plugin2 plugin;
+
+    public PlayerDeathListener (Plugin2 plugin) {
+        this.plugin = plugin;
+
+        Bukkit.getPluginManager().registerEvents(this, plugin);
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent e) {
+        if(e.getEntity().getKiller() instanceof Player){
+            Player killer = (Player)e.getEntity().getKiller();
+            Player killed = (Player)e.getEntity();
+            new BukkitRunnable(){
+
+                @Override
+                public void run() {
+                    killer.setHealth(Math.min(killer.getHealth()+8, 20));
+                    killed.setSaturation(10);
+                    killed.setFoodLevel(20);
+                }
+
+            }.runTaskLater(plugin,5);
+
+
+        }
+    }
+    @EventHandler
+    public void doesThisWork(PlayerInteractEvent e) {
+        Player clicker = e.getPlayer();
+
+        if(e.getMaterial().equals(Material.BLAZE_ROD)) {
+            if(e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK)
+                clicker.getWorld().spawnEntity(clicker.getLocation().add(0,1,0),EntityType.FIREBALL);
+        }
+    }
+    @EventHandler
+    public void damage(EntityDamageEvent e) {
+        if(e.getEntity() instanceof Player) {
+            if(e.getCause().equals(EntityDamageEvent.DamageCause.BLOCK_EXPLOSION) || e.getCause().equals(EntityDamageEvent.DamageCause.FALL)) {
+                e.setCancelled(true);
+            }
+        }
+    }
+
+
+}
